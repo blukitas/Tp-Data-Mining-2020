a520d2da79915337122b94c66066ca97deb74103
Lucas Bertolini <lucasbertolini@hotmil.com>
2020-05-25 20:08:29 -0300

    Primer checkin, primeras averiguaciones y pruebas

---
+++ 02-Vistas.js
@@ -0,0 +1,177 @@
+
+db.tweet_type.drop();
+db.createView(
+    "tweet_type",
+    "tweets_lower",
+    [{
+            $project: {
+                "_id": "$_id",
+                "is_quote": "$is_quote",
+                "is_retweet": "$is_retweet",
+                "verified": "$verified",
+            }
+        }
+    ])
+db.tweet_type.find({});
+
+db.fechas.drop();
+db.createView(
+    "fechas",
+    "tweets_lower",
+    [{
+            $project: {
+                "_id": "$_id",
+                "created_at": "$created_at",
+                "retweet_created_at": "$retweet_created_at",
+                "quoted_created_at": "$quoted_created_at",
+            }
+        }
+    ])
+db.fechas.find({});
+	
+db.user_estadisticas.drop();
+db.createView(
+    "user_estadisticas",
+    "users_mongo_covid19",
+    [{
+            $project: {
+                "_id": "$_id",
+                "statuses_count": "$statuses_count",
+                "favourites_count": "$favourites_count",
+                "listed_count": "$listed_count",
+                "friends_count": "$friends_count",
+                "followers_count": "$followers_count",
+                "verified": "$verified"
+            }
+        }
+    ])
+db.user_estadisticas.find({});
+
+db.tweet_estadisticas.drop();
+db.createView(
+    "tweet_estadisticas",
+    "tweets_lower",
+    [{
+            $project: {
+                "_id": "$_id",
+                "favorite_count": "$favorite_count",
+                "quote_count": "$quote_count",
+                "retweet_count": "$retweet_count",
+                "reply_count": "$reply_count",
+                "is_quote": "$is_quote",
+                "is_retweet": "$is_retweet",
+                "verified": "$verified"
+            }
+        }
+    ]);
+db.tweet_estadisticas.find({});
+
+db.tweet_original_estadisticas.drop();
+db.createView(
+    "tweet_original_estadisticas",
+    "tweets_lower",
+    [{
+            $project: {
+                "_id": "$_id",
+                "quoted_favorite_count": "$quoted_favorite_count",
+                "quoted_retweet_count": "$quoted_retweet_count",
+                "retweet_favorite_count": "$retweet_favorite_count",
+                "retweet_retweet_count": "$retweet_retweet_count",
+                "retweet_verified": "$retweet_verified",
+                "quoted_verified": "$quoted_verified"
+            }
+        }
+    ]);
+db.tweet_original_estadisticas.find({});
+
+db.tweet_completo_estadisticas.drop();
+db.createView(
+    "tweet_completo_estadisticas",
+    "tweets_lower",
+    [{
+            $project: {
+                "_id": "$_id",
+                "favorite_count": "$favorite_count",
+                "quote_count": "$quote_count",
+                "retweet_count": "$retweet_count",
+                "reply_count": "$reply_count",
+                "is_quote": "$is_quote",
+                "is_retweet": "$is_retweet",
+                "verified": "$verified",
+                "quoted_favorite_count": "$quoted_favorite_count",
+                "quoted_retweet_count": "$quoted_retweet_count",
+                "retweet_favorite_count": "$retweet_favorite_count",
+                "retweet_retweet_count": "$retweet_retweet_count",
+                "retweet_verified": "$retweet_verified",
+                "quoted_verified": "$quoted_verified"
+            }
+        }
+    ]);
+db.tweet_completo_estadisticas.find({});
+
+
+db.tweet_users_original.drop();
+db.createView(
+    "tweet_users_original",
+    "tweets_lower",
+    [{
+            $project: {
+                "_id": "$_id",
+                "retweet_user_id": "$retweet_user_id",
+                "quoted_user_id": "$quoted_user_id",
+                "quoted_followers_count": "$quoted_followers_count",
+                "quoted_friends_count": "$quoted_friends_count",
+                "quoted_statuses_count": "$quoted_statuses_count",
+                "retweet_followers_count": "$retweet_followers_count",
+                "retweet_friends_count": "$retweet_friends_count",
+                "retweet_statuses_count": "$retweet_statuses_count"
+            }
+        }
+    ]);
+db.tweet_users_original.find({});
+
+
+db.tweet_hashtags.drop();
+db.createView(
+    "tweet_hashtags",
+    "tweets_lower",
+    [{
+            $project: {
+                "_id": "$_id",
+                "user_id": "$user_id",
+                "hashtags": "$hashtags"
+            }
+        }, {
+            $unwind: "$hashtags"
+        }
+    ]);
+db.tweet_hashtags.find({});
+
+
+db.tweet_count_hashtags.drop();
+db.createView(
+    "tweet_count_hashtags",
+    "tweets_lower",
+    [{
+            $unwind: "$hashtags"
+        }, {
+            "$match": {
+                "hashtags": {
+                    "$ne": null
+                }
+            }
+        }, {
+            $group: {
+                _id: "$hashtags",
+                hashtag: { $min: "$hashtags" },
+                count: {
+                    $sum: 1
+                },
+            }
+        }, {
+            $sort: {
+                count: -1
+            }
+        }
+    ]);
+db.tweet_count_hashtags.find({});
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Tp-Data-Mining-2020
-Repositorio para el trabajo práctico de Data Mining. Grupo 11, Integrantes: 
+Repositorio para el trabajo práctico de Data Mining. Grupo 11
+
+## Integrantes: 
+
+* Vilca, Stella
+* Di Paola, Matías
+* Bertolini, Lucas
+
+## Datos:
+
+* Dos data sets. Tweets y Usuarios.
+* 25440 personas distintas twitteando.
+* 25435 personas distintas archivo de personas.
+* Tweets no protegidos, todos en español.
+
+## Contenido:
+
+00-consultas_varias.js -> Queries en mongo db
+01-cambios.js -> Transformaciones de mongo (To lowercase de la mayoria de los campos).
+02-Vistas.js -> Vistas parciales, acotando la información a subsets con cierta relevanta
+	- Hashtag por usuario
+	- Cantidad de cada hashtag
+	- Tipos de tweet
+	- Estadísticas de tweet
+	- Datos de usuario
+	- Datos de los tweets que fueron citados y/o retweeteados.
+	
+## Links útiles:
+
+### TP1: 
+
+https://docs.google.com/document/d/1YxDw2wK7fw67uvL43v5auMCGHOVleTnF/edit#
+
+### Datos:
+
+https://drive.google.com/open?id=1JJboF06KyYgJBI7NtU2MwI1EjqLbqvn6
+
+### Estructura de datos:
+
+* https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/intro-to-tweet-json
+* https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/user-object
+
