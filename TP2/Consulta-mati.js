d7f325298f5619509b818931b12398eaa71c13ba
Lucas Bertolini <lucasbertolini@hotmil.com>
2020-06-29 20:52:27 -0300

    Agregar tipo a tweets_lower, consulta mati

---
+++ TP2/Consulta-mati.js
@@ -0,0 +1,67 @@
+db.tweets_lower.aggregate([{
+        "$project": {
+            "_id": 1,
+            "location": 1,
+            "country": 1,
+            "created_at": 1,
+            "created_at_dia": {$dayOfWeek: "$created_at"},
+            "created_at_hora": {$hour: "$created_at"},
+            "created_at_momento": {
+                $switch: {
+                      branches: [
+                         { case: { $and: [
+                                        { "$gt": [{$hour: "$created_at"}, 5]},
+                                        { "$lt": [{$hour: "$created_at"}, 13]} 
+                                        ]}, then: "Mañana" },
+                         { case: { $and: [
+                                        { "$gt": [{$hour: "$created_at"}, 12]},
+                                        { "$lt": [{$hour: "$created_at"}, 20]} 
+                                        ]}, then: "Tarde" },
+                         { case: { $or: [
+                                        { "$gt": [{$hour: "$created_at"}, 19]},
+                                        { "$lt": [{$hour: "$created_at"}, 6]} 
+                                        ]}, then: "Noche" },
+                      ],
+                      default: "Otro"
+                   }
+                },
+            "tipo": {
+                $switch: {
+                      branches: [
+                         { case: { $and: [
+                                        { "$eq": ["$is_retweet", true]},
+                                        { "$eq": ["$is_quote", true]} 
+                                        ]}, then: "RT/QT" },
+                         { case: { $and: [
+                                        { "$eq": ["$is_retweet", true]},
+                                        { "$eq": ["$is_quote", false]} 
+                                        ]}, then: "RT" },
+                         { case: { $and: [
+                                        { "$eq": ["$is_retweet", false]},
+                                        { "$eq": ["$is_quote", true]} 
+                                        ]}, then: "QT" },
+                         { case: { $and: [
+                                        { "$eq": ["$is_retweet", false]},
+                                        { "$eq": ["$is_quote", false]} 
+                                        ]}, then: "TW" },
+                      ],
+                      default: "Otro"
+                   }
+                },
+            "hashtag": "$hashtags"
+        }
+    }, {
+        "$unwind": "$hashtag"
+    }, {
+        "$match": { "hashtag": { "$ne": null } }
+    },{
+        "$match": { "$and": [
+                    { 'hashtag': { "$not": /.*covid*./ } }, 
+                    { 'hashtag': { "$not": /.*corona*./ }}, 
+                    { 'hashtag': { "$not": /.*coronavirus*./ }},  
+                    { 'hashtag': { "$not": /.*cuarentena*./ }}, 
+                    { 'hashtag': { "$not": /.*casa*./ }}, 
+                    ]
+                   }                    
+    }
+])
---
+++ TP2/mongo/07-tipos.js
@@ -0,0 +1,43 @@
+
+db.tweets_lower.aggregate([{
+        "$project": {
+            "_id": 1,
+            "is_retweet": 1,
+            "is_quote": 1,
+            "tipo": {
+                $switch: {
+                      branches: [
+                         { case: { $and: [
+                                        { "$eq": ["$is_retweet", true]},
+                                        { "$eq": ["$is_quote", true]} 
+                                        ]}, then: "RT/QT" },
+                         { case: { $and: [
+                                        { "$eq": ["$is_retweet", true]},
+                                        { "$eq": ["$is_quote", false]} 
+                                        ]}, then: "RT" },
+                         { case: { $and: [
+                                        { "$eq": ["$is_retweet", false]},
+                                        { "$eq": ["$is_quote", true]} 
+                                        ]}, then: "QT" },
+                         { case: { $and: [
+                                        { "$eq": ["$is_retweet", false]},
+                                        { "$eq": ["$is_quote", false]} 
+                                        ]}, then: "TW" },
+                      ],
+                      default: "Otro"
+                   }
+                },
+        }
+    }
+]).forEach(function (doc) {
+    db.tweets_lower.update(
+    {_id: doc._id
+    }, 
+    {$set: {
+        "tipo": doc.tipo
+        }
+    },{
+        upsert: false,
+        multi: true
+    })
+});
