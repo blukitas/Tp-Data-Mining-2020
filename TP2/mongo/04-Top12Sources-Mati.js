34a56bee5106b83af63d941860e41d00faed1ef4
Lucas Bertolini <lucasbertolini@hotmil.com>
2020-07-11 18:59:42 -0300

    Script sources Mati

---
+++ TP2/mongo/04-Top12Sources-Mati.js
@@ -0,0 +1,23 @@
+algunos posteos:
+
+tipos de datos primitivos
+complejidad algoritmica
+
+microfront ends
+render dinámico de componentes
+como integrar
+
+pentaho, 
+	pequeña guía básica
+	paralelismo
+	distintos objetos interesantes
+	estructura de loggeo
+
+algo de data science?
+
+- politicas de ejcucion. Error que no puede ejcutar powershell
+	Set-ExecutionPolicy RemoteSignedSet-ExecutionPolicy RemoteSigned
+	
+	
+	
+	
